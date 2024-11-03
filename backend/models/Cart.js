99cafd83aac128a90d1c8dba6af4184acaf43c4d
joshnagurram <gurramjoshna823@gmail.com>
2024-11-04 02:48:02 +0530

    Add backend code

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -3,4 +3,5 @@ const mongoose = require('mongoose');
 const cartSchema = new mongoose.Schema({
     productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' } // Ensure you have this reference
 });
+
 module.exports = mongoose.model('Cart', cartSchema);
